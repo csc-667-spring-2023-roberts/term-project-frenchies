cf16a6ec018d4225b64963d6aa1fff6c4ba1daf3
Coline <coline.seguret@epitech.eu>
2023-03-15 04:43:29 +0100

    feat: init project basics

---
+++ backend/src/routes/__tests__/routes.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest';
+import app from '../../app';
+
+describe('App', () => {
+  it('should return 200 OK status for the home page', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+  });
+
+  it('should return 200 OK status for the about page', async () => {
+    const response = await request(app).get('/about');
+    expect(response.status).toBe(200);
+  });
+});
